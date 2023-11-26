a50b0344db67b49f38eeee27c93dc0d7425b6609
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2023-11-26 21:52:50 +0100

    Add project files.

---
+++ Drones.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Services.Exam._1stEval
+{
+    internal class Drones
+    {
+        public static readonly object l = new();
+        static readonly object gate = new();
+        public static bool FinalizeDrone2 { get; set; }
+        public static bool FinalizeDrone1 { get; set; }
+
+        public static void RandomInfo()
+        {
+            Console.SetCursorPosition(1, 10);
+            Console.WriteLine(new string(' ', 1000));
+
+            var item = Program.threads[new Random().Next(Program.threads.Length)];
+
+
+            Console.SetCursorPosition(1, 10);
+            Console.WriteLine(string.Format("{0} {1} {2}", Regex.Replace(item.ProcessName.ToString(), "(?<=^.{15}).*", "..."),
+                item.Id.ToString(),
+                item.Threads.Count.ToString()) + "\t");
+
+            for (var index = 0; index < ((item.Modules.Count >= 10) ? 10 : item.Modules.Count); index++)
+            {
+                var processModule = item.Modules[index];
+                Console.WriteLine(processModule.ModuleName);
+            }
+        }
+
+        public delegate void MyDelegate();
+
+        public static void ExceptionControl(MyDelegate myDelegate)
+        {
+            try
+            {
+                myDelegate();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Panic Error!!" + e.Message);
+            }
+        }
+
+        public static void FlyDrone(int droneNumber, int row)
+        {
+            var speed = new Random().Next(100, 200);
+            var format = droneNumber==1?"*":"+";
+            for (var i = 1; i <= 20; i++)
+            {
+                Thread.Sleep(speed);
+
+                if (droneNumber == 1 && FinalizeDrone1)
+                    return;
+                if (droneNumber == 2 && FinalizeDrone2)
+                    return;
+                        lock (l)
+                        {
+                            while (Program.mustWait)
+                            {
+                                Thread.Sleep(100);
+                                Monitor.Wait(l);
+                            }
+
+                            Console.SetCursorPosition(i, row);
+                            Console.Write(format);        
+                        }
+                        
+                    
+                    
+                
+            }
+            lock (gate)
+            {
+                for (var i = 21; i <= 30; i++)
+                {
+                    if (droneNumber == 1 && FinalizeDrone1)
+                        return;
+                    if (droneNumber == 2 && FinalizeDrone2)
+                        return;
+                    Thread.Sleep(speed);
+                    Console.SetCursorPosition(i, 6);
+                    Console.Write(format);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+
+namespace Services.Exam._1stEval
+{
+    internal abstract class Program
+    {
+        public static Process[] threads = Process.GetProcesses();
+        public static bool mustWait = false;
+
+        private static void Main()
+        {
+            var mustExit = false;
+
+            var threadDrone1 = new Thread(() => { Drones.FlyDrone(1, 5); });
+            threadDrone1.Start();
+            var threadDrone2 = new Thread(() => { Drones.FlyDrone(2, 7); });
+            threadDrone2.Start();
+            while (!mustExit)
+            {
+                if (!threadDrone1.IsAlive && !threadDrone2.IsAlive)
+                {
+                    return;
+                }
+                if (Console.KeyAvailable) //if there’s a key in keyboard’s buffer
+                {
+                    var key = Console.ReadKey(true);
+                    switch (key.KeyChar)
+                    {
+                        case 'p':
+                        
+                            PauseThread();
+                            break;
+
+                        case 'c':
+                            ResumeThread();
+                            break;
+                        
+                        case '1':
+                            Drones.FinalizeDrone1 = true;
+                            break;
+                        case '2': //finalize drone 2
+                            Drones.FinalizeDrone2 = true;
+                            break;
+                        case 'o':
+                            mustExit = true;
+                            break;
+                        case 'i':
+                            Drones.ExceptionControl(Drones.RandomInfo);
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static void PauseThread()
+        {
+            
+            lock (Drones.l)
+            {
+                mustWait = true;
+                
+            }
+        }
+
+        public static void ResumeThread()
+        {
+            
+            lock (Drones.l)
+            {
+                mustWait = false;
+                Monitor.PulseAll(Drones.l);
+                
+            }
+        }
+    }
+}
